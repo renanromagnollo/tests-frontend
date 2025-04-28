64f06ed33c4ee7f8c5731a36b3c0479ecaf86ebc
renanromagnollo <renanromagnollo@gmail.com>
2025-04-28 09:30:22 -0300

    feat: add TaskForm

---
+++ src/app/_components/TaskForm.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { useState } from 'react'
+
+type TaskFormProps = {
+  onAddTask: (title: string) => void
+}
+
+export default function TaskForm({ onAddTask }: TaskFormProps) {
+
+  const [title, setTitle] = useState("")
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    if (title.trim() !== "") {
+      onAddTask(title)
+      setTitle("")
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder='Nova tarefa'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button type="submit">Adicionar</button>
+    </form>
+  )
+}
